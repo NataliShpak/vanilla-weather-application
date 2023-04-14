931e53b1cd37a1511cc3959069e5767d3d581391
NataliiaShpak <shpakn@ukr.net>
2023-04-14 11:09:58 +0200

    Added relevant icon from the API

--- src/app.js
+++ src/app.js
@@ -28,17 +28,19 @@ function displayTemperature(response) {
      let humidityElement = document.querySelector("#humidity");
      let windElement = document.querySelector("#wind");
      let dateElement = document.querySelector("#date");
+     let iconElement = document.querySelector("#icon")
      temperatureElement.innerHTML = Math.round(response.data.main.temp);
      cityElement.innerHTML = response.data.name;
      descriptionElement.innerHTML = response.data.weather[0].description;
      humidityElement.innerHTML = response.data.main.humidity;
      windElement.innerHTML = Math.round(response.data.wind.speed);
      dateElement.innerHTML = formatDate(response.data.dt * 1000);
-}
+     iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`);
+     iconElement.setAttribute("alt", response.data.weather[0].description);
+    }
 
 let apiKey = "3c29f5c0dd37893b3303fa1d3b8af0fc";
-// let cityName = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
